f38ef4776d72ee9eca84683d73710c329ef147b4
matansharon <matca2953@gmail.com>
2024-05-12 11:36:45 +0300

    testing the display chat history

--- main.py
+++ main.py
@@ -34,17 +34,21 @@ class Message():
     # Get the collection.
 def init():
     
-        
+    anthropic_client=anthropic.Anthropic(
+        api_key=os.environ.get("ANTHROPIC_API_KEY")
+    )
+    
     st.session_state['init']=True
-    client = chromadb.PersistentClient(path='chroma_storage')
-    collection = client.get_collection(name='file1_collection')
+    db_client = chromadb.PersistentClient(path='chroma_storage')
+    collection = db_client.get_collection(name='file1_collection')
     st.session_state['collection']=collection
-    st.session_state['client']=client
+    st.session_state['anthropic_client']=anthropic_client
     st.session_state['input_usage']=0
     st.session_state['output_usage']=0
     st.session_state['total_usage']=0
     st.session_state['existing_files']=set()
-
+    st.session_state['chat_history']=[]
+    st.session_state['chat_history'].append(Message("Hello, I am an AI assistant. How can I help you today?","ai"))
     
 
 
@@ -69,10 +73,9 @@ def get_anthropic_response(query: str) -> str:
         
     
     context=results["documents"][0]
-    client=anthropic.Anthropic(
-        api_key=os.environ.get("ANTHROPIC_API_KEY")
-    )
-    response = client.messages.create(
+    
+    
+    response = st.session_state['anthropic_client'].messages.create(
         # model="claude-3-opus-20240229",
         model='claude-3-haiku-20240307',
         max_tokens=1000,
@@ -95,8 +98,7 @@ def get_anthropic_response(query: str) -> str:
     st.session_state.input_usage+=response.usage.input_tokens
     st.session_state.output_usage+=response.usage.output_tokens
     st.session_state.total_usage+=st.session_state.input_usage+st.session_state.output_usage
-    display_chat_history()
-    
+
 #--------------------------------------------------------------------------------------------------------------------------------#
 #--------------------------------------------------------------------------------------------------------------------------------#
 def write_side_bar():
@@ -116,18 +118,18 @@ def write_side_bar():
 #--------------------------------------------------------------------------------------------------------------------------------#
 #--------------------------------------------------------------------------------------------------------------------------------#
 def display_chat_history():
-    if 'chat_history' not in st.session_state:
-        st.session_state['chat_history']=[]
+
         
-        st.session_state['chat_history'].append(Message("Hello, I am an AI assistant. How can I help you today?","ai"))
-    ai=st.chat_message("ai")
-    user=st.chat_message("user")
-    for i in st.session_state['chat_history']:
-        if i.get_type()=='ai':
-            ai.write(i.get_content())
-        elif i.get_type()=='user':
-            st.write("in user")
-            user.write(i.get_content())
+    st.write("number of objects in chat_history: ",len(st.session_state['chat_history']))
+    if 'chat_history' in st.session_state:
+        ai=st.chat_message("ai")
+        user=st.chat_message("user")
+        for i in st.session_state['chat_history']:
+            if i.get_type()=='ai':
+                st.write(i.get_content())
+            elif i.get_type()=='user':
+
+                st.write(i.get_content())
     
 def main() -> None:
     if 'init' not in st.session_state:
@@ -142,21 +144,22 @@ def main() -> None:
     write_side_bar()
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Load documents from a directory into a Chroma collection"
-    )
-    parser.add_argument(
-        "--persist_directory",
-        type=str,
-        default="chroma_storage",
-        help="The directory where you want to store the Chroma collection",
-    )
-    parser.add_argument(
-        "--collection_name",
-        type=str,
-        default="documents_collection",
-        help="The name of the Chroma collection",
-    )
-    # Parse arguments
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser(
+    #     description="Load documents from a directory into a Chroma collection"
+    # )
+    # parser.add_argument(
+    #     "--persist_directory",
+    #     type=str,
+    #     default="chroma_storage",
+    #     help="The directory where you want to store the Chroma collection",
+    # )
+    # parser.add_argument(
+    #     "--collection_name",
+    #     type=str,
+    #     default="documents_collection",
+    #     help="The name of the Chroma collection",
+    # )
+    # # Parse arguments
+    # args = parser.parse_args()
+    
     main()
