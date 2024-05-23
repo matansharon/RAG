fafc81898dbd6cb69676c81dfa3b45e6d99a062f
matansharon <matca2953@gmail.com>
2024-05-23 07:09:38 +0300

    adding the testing auth

--- test_auth.py
+++ test_auth.py
@@ -17,11 +17,3 @@ authenticator = Authenticate(
 )
 authenticator.login()
 
-if st.session_state["authentication_status"]:
-    authenticator.logout('Logout', 'main')
-    st.write(f'Welcome *{st.session_state["name"]}*')
-    st.title('Some content')
-elif st.session_state["authentication_status"] == False:
-    st.error('Username/password is incorrect')
-elif st.session_state["authentication_status"] == None:
-    st.warning('Please enter your username and password')
